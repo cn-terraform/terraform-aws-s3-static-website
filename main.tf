47261c13153f8b439a7e8bc67a70d8fe0a4b229d
Maria Florencia Caro <caromflorencia@gmail.com>
2021-07-22 19:45:42 +0100

    Using default KMS key for S3

--- main.tf
+++ main.tf
@@ -21,8 +21,7 @@ resource "aws_s3_bucket" "log_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "aws/s3"
-        sse_algorithm     = "aws:kms"
+        sse_algorithm = "aws:kms"
       }
     }
   }
