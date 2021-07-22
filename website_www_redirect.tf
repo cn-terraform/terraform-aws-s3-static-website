f8cec2df0ca60f1fa971616ebb69fa10cefcba4c
Maria Florencia Caro <caromflorencia@gmail.com>
2021-07-22 19:47:55 +0100

    Using default KMS key for S3

--- website_www_redirect.tf
+++ website_www_redirect.tf
@@ -33,8 +33,7 @@ resource "aws_s3_bucket" "www_website" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "aws/s3"
-        sse_algorithm     = "aws:kms"
+        sse_algorithm = "aws:kms"
       }
     }
   }
