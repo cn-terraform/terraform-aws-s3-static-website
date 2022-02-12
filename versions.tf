ac7011e950c1b1657c034c624cbc28c1b58eee78
Julian Nonino <noninojulian@gmail.com>
2022-02-12 23:04:03 +0000

    Update AWS privider

--- versions.tf
+++ versions.tf
@@ -2,8 +2,9 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.74"
+      source                = "hashicorp/aws"
+      version               = ">= 3.74"
+      configuration_aliases = [aws.main, aws.acm_provider]
     }
   }
 }
