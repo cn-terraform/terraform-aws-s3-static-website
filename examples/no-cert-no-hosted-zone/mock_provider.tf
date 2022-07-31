ed19b687881e412b53a8a1c6ff1e02f4c69f0b7e
Julian Nonino <noninojulian@gmail.com>
2022-07-31 21:59:31 +0100

    Use localstack for testing

--- examples/no-cert-no-hosted-zone/mock_provider.tf
+++ examples/no-cert-no-hosted-zone/mock_provider.tf
@@ -9,23 +9,63 @@ terraform {
 }
 
 provider "aws" {
+  alias                       = "main"
   region                      = "us-east-1"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
   s3_use_path_style           = true
-  access_key                  = "mock_access_key" # tfsec:ignore:general-secrets-no-plaintext-exposure
-  secret_key                  = "mock_secret_key" # tfsec:ignore:general-secrets-no-plaintext-exposure
-  alias                       = "main"
+
+  endpoints {
+    apigateway     = "http://localstack:4566"
+    cloudformation = "http://localstack:4566"
+    cloudwatch     = "http://localstack:4566"
+    dynamodb       = "http://localstack:4566"
+    es             = "http://localstack:4566"
+    firehose       = "http://localstack:4566"
+    iam            = "http://localstack:4566"
+    kinesis        = "http://localstack:4566"
+    lambda         = "http://localstack:4566"
+    route53        = "http://localstack:4566"
+    redshift       = "http://localstack:4566"
+    s3             = "http://localstack:4566"
+    secretsmanager = "http://localstack:4566"
+    ses            = "http://localstack:4566"
+    sns            = "http://localstack:4566"
+    sqs            = "http://localstack:4566"
+    ssm            = "http://localstack:4566"
+    stepfunctions  = "http://localstack:4566"
+    sts            = "http://localstack:4566"
+  }
 }
 
 provider "aws" {
+  alias                       = "acm_provider"
   region                      = "us-east-1"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
   s3_use_path_style           = true
-  access_key                  = "mock_access_key" # tfsec:ignore:general-secrets-no-plaintext-exposure
-  secret_key                  = "mock_secret_key" # tfsec:ignore:general-secrets-no-plaintext-exposure
-  alias                       = "acm_provider"
+
+  endpoints {
+    apigateway     = "http://localstack:4566"
+    cloudformation = "http://localstack:4566"
+    cloudwatch     = "http://localstack:4566"
+    dynamodb       = "http://localstack:4566"
+    es             = "http://localstack:4566"
+    firehose       = "http://localstack:4566"
+    iam            = "http://localstack:4566"
+    kinesis        = "http://localstack:4566"
+    lambda         = "http://localstack:4566"
+    route53        = "http://localstack:4566"
+    redshift       = "http://localstack:4566"
+    s3             = "http://localstack:4566"
+    secretsmanager = "http://localstack:4566"
+    ses            = "http://localstack:4566"
+    sns            = "http://localstack:4566"
+    sqs            = "http://localstack:4566"
+    ssm            = "http://localstack:4566"
+    stepfunctions  = "http://localstack:4566"
+    sts            = "http://localstack:4566"
+  }
 }
